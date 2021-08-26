303e270ae9368c5488d057bf80c757b20a289bac
xiaozhizhen <2742400566@qq.com>
2021-08-26 13:43:18 +0100

    8.26存档

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<el-row class="tac">
+  <el-col :span="3">
+    <el-menu>
+      <div class="title">
+        <i class="el-icon-coffee"></i>
+        Avilla dashboard
+      </div>
+     <el-menu-item index="1">
+        <i class="el-icon-setting"></i>
+        <template #title>状态</template>
+      </el-menu-item>
+    </el-menu>
+  </el-col>
+</el-row>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+  export default{
+    data(){
+      return{
+        message: "hello"
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.el-icon-coffee{
+  color: #8cc269;
 }
 </style>
